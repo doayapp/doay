5b595013bfc01d41017142decc4faccbb050ac69
Doay <202287678+doooay@users.noreply.github.com>
2025-05-13 16:14:21 +0800

    add isDev

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-document.addEventListener('contextmenu', e => e.preventDefault())
+const isDev = import.meta.env.MODE === 'development'
+if (!isDev) document.addEventListener('contextmenu', e => e.preventDefault())
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
