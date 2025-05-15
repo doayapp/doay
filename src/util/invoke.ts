84ab7e1d42fb6132530d0f8475c001fa152b1e41
Doay <202287678+doooay@users.noreply.github.com>
2025-05-16 00:15:10 +0800

    fix

--- src/util/invoke.ts
+++ src/util/invoke.ts
@@ -49,237 +49,237 @@ export async function invokeString(apiName: string, options: any = {}) {
 }
 
 export async function getDoayAppDir(): Promise<string> {
-    return invokeString('get_doay_app_dir')
+    return await invokeString('get_doay_app_dir')
 }
 
-export function restartRay() {
-    return invokeBool('restart_ray')
+export async function restartRay() {
+    return await invokeBool('restart_ray')
 }
 
 export async function checkPortAvailable(port: number) {
-    return invokeBool('check_port_available', {port})
+    return await invokeBool('check_port_available', {port})
 }
 
 export async function startSpeedTestServer(port: number, filename: string) {
-    return invokeBool('start_speed_test_server', {port, filename})
+    return await invokeBool('start_speed_test_server', {port, filename})
 }
 
 export async function stopSpeedTestServer(port: number) {
-    return invokeBool('stop_speed_test_server', {port})
+    return await invokeBool('stop_speed_test_server', {port})
 }
 
 export async function readAppConfig(): Promise<AppConfig | undefined> {
-    return safeInvoke('get_config_json')
+    return await safeInvoke('get_config_json')
 }
 
 export async function saveAppConfig(cmd: string, value: string | number | boolean) {
-    return invokeBool(cmd, {value})
+    return await invokeBool(cmd, {value})
 }
 
 export async function readRayConfig(): Promise<any> {
-    return safeInvoke('read_ray_config')
+    return await safeInvoke('read_ray_config')
 }
 
 export async function appElapsed() {
-    return safeInvoke('app_elapsed')
+    return await safeInvoke('app_elapsed')
 }
 
 export async function isQuietMode() {
-    return invokeBool('is_quiet_mode')
+    return await invokeBool('is_quiet_mode')
 }
 
 export async function saveRayConfig(content: any) {
-    return invokeBool('save_ray_config', {content: jsonStringify(content)})
+    return await invokeBool('save_ray_config', {content: jsonStringify(content)})
 }
 
 export async function saveProxyPac(content: string) {
-    return invokeBool('save_proxy_pac', {content})
+    return await invokeBool('save_proxy_pac', {content})
 }
 
 export async function saveTextFile(path: string, content: string) {
-    return invokeBool('save_text_file', {path, content})
+    return await invokeBool('save_text_file', {path, content})
 }
 
 export async function openWebServerDir() {
-    return invokeBool('open_web_server_dir')
+    return await invokeBool('open_web_server_dir')
 }
 
 export async function clearLogAll() {
-    return invokeBool('clear_log_all')
+    return await invokeBool('clear_log_all')
 }
 
 export async function readLogList(): Promise<LogList | undefined> {
-    return safeInvoke('read_log_list')
+    return await safeInvoke('read_log_list')
 }
 
 export async function readLogFile(filename: string, reverse: boolean = true, start: number = -1): Promise<LogContent | undefined> {
-    return safeInvoke('read_log_file', {filename, reverse, start})
+    return await safeInvoke('read_log_file', {filename, reverse, start})
 }
 
 async function readConf(filename: string) {
-    return safeInvoke('read_conf', {filename})
+    return await safeInvoke('read_conf', {filename})
 }
 
 async function saveConf(filename: string, content: any) {
-    return invokeBool('save_conf', {filename, content: jsonStringify(content)})
+    return await invokeBool('save_conf', {filename, content: jsonStringify(content)})
 }
 
 export async function saveSpeedTestConf(filename: string, content: any) {
-    return invokeBool('save_speed_test_conf', {filename, content: jsonStringify(content)})
+    return await invokeBool('save_speed_test_conf', {filename, content: jsonStringify(content)})
 }
 
 export async function readRayCommonConfig(): Promise<RayCommonConfig | undefined> {
-    return readConf('ray_common_config.json')
+    return await readConf('ray_common_config.json')
 }
 
 export async function saveRayCommonConfig(content: RayCommonConfig) {
-    return saveConf('ray_common_config.json', content)
+    return await saveConf('ray_common_config.json', content)
 }
 
 export async function readServerList(): Promise<ServerList | undefined> {
-    return readConf('server.json')
+    return await readConf('server.json')
 }
 
 export async function saveServerList(content: ServerList) {
-    return saveConf('server.json', content)
+    return await saveConf('server.json', content)
 }
 
 export async function readSubscriptionList(): Promise<SubscriptionList | undefined> {
-    return readConf('subscription.json')
+    return await readConf('subscription.json')
 }
 
 export async function saveSubscriptionList(content: SubscriptionList) {
-    return saveConf('subscription.json', content)
+    return await saveConf('subscription.json', content)
 }
 
 export async function readRuleConfig(): Promise<RuleConfig | undefined> {
-    return readConf('rule_config.json')
+    return await readConf('rule_config.json')
 }
 
 export async function saveRuleConfig(content: RuleConfig) {
-    return saveConf('rule_config.json', content)
+    return await saveConf('rule_config.json', content)
 }
 
 export async function readRuleDomain(): Promise<RuleDomain | undefined> {
-    return readConf('rule_domain.json')
+    return await readConf('rule_domain.json')
 }
 
 export async function saveRuleDomain(content: RuleDomain) {
-    return saveConf('rule_domain.json', content)
+    return await saveConf('rule_domain.json', content)
 }
 
 export async function readRuleModeList(): Promise<RuleModeList | undefined> {
-    return readConf('rule_mode_list.json')
+    return await readConf('rule_mode_list.json')
 }
 
 export async function saveRuleModeList(content: RuleModeList) {
-    return saveConf('rule_mode_list.json', content)
+    return await saveConf('rule_mode_list.json', content)
 }
 
 export async function readDnsConfig(): Promise<DnsConfig | undefined> {
-    return readConf('dns_config.json')
+    return await readConf('dns_config.json')
 }
 
 export async function saveDnsConfig(content: DnsConfig) {
-    return saveConf('dns_config.json', content)
+    return await saveConf('dns_config.json', content)
 }
 
 export async function readDnsModeList(): Promise<DnsModeList | undefined> {
-    return readConf('dns_mode_list.json')
+    return await readConf('dns_mode_list.json')
 }
 
 export async function saveDnsModeList(content: DnsModeList) {
-    return saveConf('dns_mode_list.json', content)
+    return await saveConf('dns_mode_list.json', content)
 }
 
 export async function readDnsTableList(): Promise<DnsTableList | undefined> {
-    return readConf('dns_table_list.json')
+    return await readConf('dns_table_list.json')
 }
 
 export async function saveDnsTableList(content: DnsTableList) {
-    return saveConf('dns_table_list.json', content)
+    return await saveConf('dns_table_list.json', content)
 }
 
 export async function readSpeedTestConfig(): Promise<SpeedTestConfig | undefined> {
-    return readConf('speed_test_config.json')
+    return await readConf('speed_test_config.json')
 }
 
 export async function saveSpeedTestConfig(content: SpeedTestConfig) {
-    return saveConf('speed_test_config.json', content)
+    return await saveConf('speed_test_config.json', content)
 }
 
 export async function getSysInfoJson() {
-    return safeInvoke('get_sys_info_json')
+    return await safeInvoke('get_sys_info_json')
 }
 
 export async function getLoadAverageJson() {
-    return safeInvoke('get_load_average_json')
+    return await safeInvoke('get_load_average_json')
 }
 
 export async function getProcessesJson(keyword: string) {
-    return safeInvoke('get_processes_json', {keyword})
+    return await safeInvoke('get_processes_json', {keyword})
 }
 
 export async function getDisksJson() {
-    return safeInvoke('get_disks_json')
+    return await safeInvoke('get_disks_json')
 }
 
 export async function getNetworksJson() {
-    return safeInvoke('get_networks_json')
+    return await safeInvoke('get_networks_json')
 }
 
 export async function getComponentsJson() {
-    return safeInvoke('get_components_json')
+    return await safeInvoke('get_components_json')
 }
 
 export async function killProcessByPid(pid: number) {
-    return invokeBool('kill_process_by_pid', {pid})
+    return await invokeBool('kill_process_by_pid', {pid})
 }
 
 export async function downloadLargeFile(url: string, filepath: string, proxyUrl: string, userAgent: string, timeout: number = 60 * 30) {
-    return safeInvoke('download_large_file', {url, filepath, proxyUrl, userAgent, timeout})
+    return await safeInvoke('download_large_file', {url, filepath, proxyUrl, userAgent, timeout})
 }
 
 export async function pingTest(url: string, proxyUrl: string, userAgent: string, count: number, timeout: number = 10) {
-    return safeInvoke('ping_test', {url, proxyUrl, userAgent, count, timeout})
+    return await safeInvoke('ping_test', {url, proxyUrl, userAgent, count, timeout})
 }
 
 export async function jitterTest(url: string, proxyUrl: string, userAgent: string, count: number, timeout: number = 10) {
-    return safeInvoke('jitter_test', {url, proxyUrl, userAgent, count, timeout})
+    return await safeInvoke('jitter_test', {url, proxyUrl, userAgent, count, timeout})
 }
 
 export async function downloadSpeedTest(url: string, proxyUrl: string, userAgent: string, timeout: number = 60 * 20) {
-    return safeInvoke('download_speed_test', {url, proxyUrl, userAgent, timeout})
+    return await safeInvoke('download_speed_test', {url, proxyUrl, userAgent, timeout})
 }
 
 export async function uploadSpeedTest(url: string, proxyUrl: string, userAgent: string, size: number, timeout: number = 60 * 10) {
-    return safeInvoke('upload_speed_test', {url, proxyUrl, userAgent, size, timeout})
+    return await safeInvoke('upload_speed_test', {url, proxyUrl, userAgent, size, timeout})
 }
 
 export async function fetchResponseHeaders(url: string, proxyUrl: string, userAgent: string = navigator.userAgent, timeout: number = 10) {
-    return safeInvoke('fetch_response_headers', {url, proxyUrl, userAgent, timeout})
+    return await safeInvoke('fetch_response_headers', {url, proxyUrl, userAgent, timeout})
 }
 
 export async function fetchTextContent(url: string, proxyUrl: string, userAgent: string = navigator.userAgent, timeout: number = 10) {
-    return safeInvoke('fetch_text_content', {url, proxyUrl, userAgent, timeout})
+    return await safeInvoke('fetch_text_content', {url, proxyUrl, userAgent, timeout})
 }
 
 export async function fetchGet(url: string, isProxy: boolean = false, userAgent: string = navigator.userAgent, timeout: number = 10) {
-    return safeInvoke('fetch_get', {url, isProxy, userAgent, timeout})
+    return await safeInvoke('fetch_get', {url, isProxy, userAgent, timeout})
 }
 
 export async function startScanPorts(host: string, startPort: number, endPort: number, maxThreads: number, timeoutMs: number = 500) {
-    return safeInvoke('start_scan_ports', {host, startPort, endPort, maxThreads, timeoutMs})
+    return await safeInvoke('start_scan_ports', {host, startPort, endPort, maxThreads, timeoutMs})
 }
 
 export async function readOpenLog() {
-    return invokeString('read_open_log')
+    return await invokeString('read_open_log')
 }
 
 export async function readTimeoutLog() {
-    return invokeString('read_timeout_log')
+    return await invokeString('read_timeout_log')
 }
 
 export async function readRefusedLog() {
-    return invokeString('read_refused_log')
+    return await invokeString('read_refused_log')
 }
