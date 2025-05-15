a94c5d618fcc7c2ed5e28224671473a9e41fbe79
Doay <202287678+doooay@users.noreply.github.com>
2025-05-15 11:25:59 +0800

    fix

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -81,41 +81,68 @@ pub fn create_main_window(app: &App) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn set_tray<R: Runtime>(app: &App<R>) -> tauri::Result<()> {
-    let quit_i = MenuItem::with_id(app, "quit", "退出 Doay", true, None::<&str>)?;
-    let menu = Menu::with_items(app, &[&quit_i])?;
-    TrayIconBuilder::new()
-        .icon(app.default_window_icon().unwrap().clone())
-        .menu(&menu)
-        .show_menu_on_left_click(false)
-        .on_menu_event(|app, event| match event.id.as_ref() {
-            "quit" => {
-                app.exit(0);
-            }
-            _ => {
-                // println!("menu item {:?} not handled", event.id);
-            }
-        })
-        .on_tray_icon_event(|tray, event| match event {
-            TrayIconEvent::Click {
-                button: MouseButton::Left,
-                button_state: MouseButtonState::Up,
-                ..
-            } => {
-                let app = tray.app_handle();
-                if let Some(window) = app.get_webview_window("main") {
-                    if let Ok(state) = window.is_minimized() {
-                        if state {
-                            let _ = window.unminimize();
-                        }
-                    }
-
-                    let _ = window.show();
-                    let _ = window.set_focus();
+    #[cfg(not(target_os = "linux"))]
+    {
+        let quit_i = MenuItem::with_id(app, "quit", "退出 Doay", true, None::<&str>)?;
+        let menu = Menu::with_items(app, &[&quit_i])?;
+        TrayIconBuilder::new()
+            .icon(app.default_window_icon().unwrap().clone())
+            .menu(&menu)
+            .show_menu_on_left_click(false)
+            .on_menu_event(|app, event| match event.id.as_ref() {
+                "quit" => {
+                    app.exit(0);
                 }
-            }
-            _ => {}
-        })
-        .build(app)?;
+                _ => {
+                    // println!("menu item {:?} not handled", event.id);
+                }
+            })
+            .on_tray_icon_event(|tray, event| match event {
+                TrayIconEvent::Click {
+                    button: MouseButton::Left,
+                    button_state: MouseButtonState::Up,
+                    ..
+                } => {
+                    let app = tray.app_handle();
+                    if let Some(window) = app.get_webview_window("main") {
+                        if let Ok(state) = window.is_minimized() {
+                            if state {
+                                let _ = window.unminimize();
+                            }
+                        }
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+                _ => {}
+            })
+            .build(app)?;
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        let show_i = MenuItem::with_id(app, "show", "显示主窗口", true, None::<&str>)?;
+        let quit_i = MenuItem::with_id(app, "quit", "退出 Doay", true, None::<&str>)?;
+        let menu = Menu::with_items(app, &[&show_i, &quit_i])?;
+
+        TrayIconBuilder::new()
+            .icon(app.default_window_icon().unwrap().clone())
+            .menu(&menu)
+            .on_menu_event(|app, event| match event.id.as_ref() {
+                "show" => {
+                    if let Some(window) = app.get_webview_window("main") {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+                "quit" => {
+                    app.exit(0);
+                }
+                _ => {}
+            })
+            .build(app)?;
+    }
+
     Ok(())
 }
 
